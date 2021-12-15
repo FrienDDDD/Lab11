5815baad472e90f2b7dd636de1bd07cd3015b8ac
FrienDDDD <friendjunk@gmail.com>
2021-12-16 00:33:31 +0700

    lab11_3.cpp complete

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,31 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include<cmath>
+#include<fstream>
+#include<cstdlib>
+#include<string>
+using namespace std;
+
+int main(){
+    
+    int N = 0 ;
+    ifstream source("score.txt");
+    string data ;
+    double sum = 0,u,sum1 = 0 ;
+    while(getline(source,data)){
+        cout << data << endl ;
+        sum += atof(data.c_str()) ;
+        sum1 += pow(atof(data.c_str()),2) ;
+        N ++ ;
+    }
+   
+    
+    cout << "Number of data = " << N << endl ;
+    cout << setprecision(3);
+    u = sum/N ;
+    cout << "Mean = " << u << endl ;
+    cout << "Standard deviation = " << sqrt((sum1/N)-pow(u,2)) ;
+    source.close() ;  
+    return 0 ;
+}
+
